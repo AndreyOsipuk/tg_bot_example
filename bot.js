b059f48edff13add72741fc66c11769e84367ea4
Осипук <andrey.o@list.ru>
2025-10-02 22:20:23 +0300

    fix load env

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: path.join(__dirname, ".env") });
 const TelegramBot = require("node-telegram-bot-api");
 // const axios = require("axios");
 
